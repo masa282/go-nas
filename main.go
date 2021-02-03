6f09140828d3f50a49e5a8deed451fac980c02cf
masa <nywhp843masato@gmail.com>
2021-02-03 19:47:48 +0900

    bag fix

--- main.go
+++ main.go
@@ -3,18 +3,57 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"html/template"
 	"io"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
 	"os"
+	"path"
 	"path/filepath"
+	"sync"
 )
 
 const (
 	defaultMaxMemory = 32 << 20 // 32 MB
 	port             = ":8080"
 )
+
+var dirname = filepath.Join(".", "data")
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch t.filename {
+	case "send.html":
+		t.once.Do(func() {
+			t.templ = template.Must(template.ParseFiles(filepath.Join("html", t.filename)))
+		})
+
+		files, err := ioutil.ReadDir(dirname)
+		if err != nil {
+			log.Println("[-]Failed to read the directory: ", dirname)
+			w.Write([]byte("Failed to read the directory"))
+			return
+		}
+
+		FileList := make(map[string]string, len(files))
+		for _, file := range files {
+			//fmt.Println(file.Name())
+			FileList[file.Name()] = path.Join(dirname, file.Name())
+		}
+
+		fmt.Println(FileList)
+		if err := t.templ.Execute(w, FileList); err != nil {
+			log.Println("[-]temple.Execute: ", err)
+		}
+	}
+}
 
 func RecvHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseMultipartForm(defaultMaxMemory)
@@ -36,12 +75,28 @@ func RecvHandler(w http.ResponseWriter, r *http.Request) {
 
 		io.Copy(dst, src)
 	}
-
-	fmt.Println("Success!")
+	fmt.Println("[+]Success!")
 	w.Write([]byte("成功！"))
 }
 
+func DataHandler(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	fmt.Println(path)
+	dir, _ := os.Getwd()
+	//fmt.Println(filepath.FromSlash(path))
+	//fmt.Println(filepath.Join(dir, path))
+	//file, err := os.Open(filepath.Join(dir, filepath.FromSlash(path)))
+	file, err := os.Open(filepath.Join(dir, path))
+	defer file.Close()
+	if err != nil {
+		log.Println("[-]Failed to Open the file: ", err)
+	}
+	io.Copy(w, file)
+}
+
 func main() {
+	http.HandleFunc("/data/", DataHandler)
+	http.Handle("/send", &templateHandler{filename: "send.html"})
 	http.HandleFunc("/recv", RecvHandler)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Someone Acessed!")
@@ -69,8 +124,8 @@ func main() {
 
 	fmt.Println(ip_port.String())
 
-	if err := http.ListenAndServe("240d:1a:6b3:3d00:94de:2131:3349:f3a0", nil); err != nil {
-		//if err := http.ListenAndServe(ip_port.String(), nil); err != nil {
-		log.Println("falied to start")
+	//if err := http.ListenAndServe("240d:1a:6b3:3d00:94de:2131:3349:f3a0", nil); err != nil {
+	if err := http.ListenAndServe(ip_port.String(), nil); err != nil {
+		log.Println("[-]Falied to start")
 	}
 }
